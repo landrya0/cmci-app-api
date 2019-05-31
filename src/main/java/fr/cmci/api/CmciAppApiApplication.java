b5e21e0be67baac6441768d2b9fa9de0f6d0d21f
Landry Agbaholou <landrya0@gmail.com>
2019-05-31 15:58:13 +0200

    Initialize app

---
+++ src/main/java/fr/cmci/api/CmciAppApiApplication.java
@@ -0,0 +1,13 @@
+package fr.cmci.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmciAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmciAppApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/cmci/api/controller/MainController.java
@@ -0,0 +1,10 @@
+package fr.cmci.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    
+
+}
